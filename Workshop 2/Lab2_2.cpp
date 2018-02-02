071f2630cb3d21313126fcba47b70c82eef332ad
Mithilan16 <Mithilan16@users.noreply.github.com>
2018-02-02 04:52:35 -0500

    Update Lab2_2.cpp

--- Workshop 2/Lab2_2.cpp
+++ Workshop 2/Lab2_2.cpp
@@ -10,27 +10,37 @@ We have not distributed our work to other students.
 #pragma comment(lib, "vfw32.lib")
 #pragma comment(lib, "comctl32.lib")
 
-int main() {
-
+int main()
+{
+	int trX, trY, blX, blY;//x and y co-ordinates for top right and bottom left co-ordinates
+	cv::Point2i topR, bottomL;//top right and bottom left points
+	char colour,exit;
+	cv::Vec3b colourVec;//3d vector to hold colour channels
+	
 	cv::namedWindow("Lab 2-2", cv::WINDOW_AUTOSIZE);
-
-	cv::Mat image(480, 640, CV_8UC3, cv::Scalar(0, 0, 0));
-
-	int x, y;
-	char color;
-
+	cv::Mat image(480, 640, CV_8UC3, cv::Vec3b(0, 0, 0));//draw image 480 by 640, all channels to 0 (black)
+	
 	for (;;) {
-		cv::imshow("Lab 2-2", image);
+		cv::imshow("Lab 2-2", image);//draw image on window
+		cv::waitKey(33);
+		
+		std::cout << "Please input an x value for the bottom left co-ordinate of a rectangle (0-480): ";
+		std::cin >> blX;
 
-		if ((char)cv::waitKey(33) >= 0) break;
+		std::cout << "Please input a y value for the bottom left co-ordinate of a rectangle (0-640): ";
+		std::cin >> blY;
 
-		std::cout << "Please input an x value for the image (1-480): ";
-		std::cin >> x;
+		std::cout << "Please input an x value for top right co-ordinate of a rectangle (0-480): ";
+		std::cin >> trX;
+		
+		std::cout << "Please input a y value for the top right co-ordinate of a rectangle (0-640): ";
+		std::cin >> trY;
 
-		std::cout << "Please input an y value for the image (1-640): ";
-		std::cin >> y;
+		//create top right and bottom left points for creation of rectangle
+		topR = cv::Point2i(trX, trY);
+		bottomL = cv::Point2i(blX, blY);
 
-		std::cout << "Please input a letter character for a color: " << std::endl;
+		std::cout << "Please input a letter character for a colour: " << std::endl;
 		std::cout << "k: black" << std::endl;
 		std::cout << "w: white" << std::endl;
 		std::cout << "r: red" << std::endl;
@@ -40,50 +50,51 @@ int main() {
 		std::cout << "y: yellow" << std::endl;
 		std::cout << "m: magenta" << std::endl;
 		std::cout << "x: random" << std::endl;
-		std::cin >> color;
-
-		if (x > 0 && x < 481 && y > 0 && y < 640) {
-			if (color == 'k') {
-				cv::Mat newImage(x, y, CV_8UC3, cv::Scalar(0, 0, 0));
-				newImage.copyTo(image(cv::Rect(0, 0, x, y)));
-			}
-			else if (color == 'w') {
-				cv::Mat newImage(x, y, CV_8UC3, cv::Scalar(255, 255, 255));
-				newImage.copyTo(image(cv::Rect(0, 0, x, y)));
-			}
-			else if (color == 'r') {
-				cv::Mat newImage(x, y, CV_8UC3, cv::Scalar(0, 0, 255));
-				newImage.copyTo(image(cv::Rect(0, 0, x, y)));
-			}
-			else if (color == 'g') {
-				cv::Mat newImage(x, y, CV_8UC3, cv::Scalar(0, 255, 0));
-				newImage.copyTo(image(cv::Rect(0, 0, x, y)));
-			}
-			else if (color == 'b') {
-				cv::Mat newImage(x, y, CV_8UC3, cv::Scalar(255, 0, 0));
-				newImage.copyTo(image(cv::Rect(0, 0, x, y)));
-			}
-			else if (color == 'c') {
-				cv::Mat newImage(x, y, CV_8UC3, cv::Scalar(255, 255, 0));
-				newImage.copyTo(image(cv::Rect(0, 0, x, y)));
-			}
-			else if (color == 'y') {
-				cv::Mat newImage(x, y, CV_8UC3, cv::Scalar(0, 255, 255));
-				newImage.copyTo(image(cv::Rect(0, 0, x, y)));
-			}
-			else if (color == 'm') {
-				cv::Mat newImage(x, y, CV_8UC3, cv::Scalar(255, 0, 255));
-				newImage.copyTo(image(cv::Rect(0, 0, x, y)));
-			}
-			else if (color == 'x') {
-				cv::Mat newImage(x, y, CV_8UC3, cv::Scalar(rand()%255, rand()%255, rand()%255));
-				newImage.copyTo(image(cv::Rect(0, 0, x, y)));
-			}
+		std::cin >> colour;
+		
+		if (colour == 'k') {//black
+			
+		}
+		else if (colour == 'w') {//white
+			colourVec = cv::Vec3b(255, 255, 255);
+		}
+		else if (colour == 'r') {//red
+			colourVec = cv::Vec3b(0, 0, 255);
+		}
+		else if (colour == 'g') {//green
+			colourVec = cv::Vec3b(0, 255, 0);
+		}
+		else if (colour == 'b') {//blue
+			colourVec = cv::Vec3b(255, 0, 0);
+		}
+		else if (colour == 'c') {//cyan
+			colourVec = cv::Vec3b(255, 255, 0);
+		}
+		else if (colour == 'y') {//yellow
+			colourVec = cv::Vec3b(0, 255, 255);
+		}
+		else if (colour == 'm') {//magenta
+			colourVec = cv::Vec3b(255, 0, 255);
+		}
+		else if (colour == 'x') {//random
+			
 		}
 
-		if ((char)cv::waitKey(33) >= 0) break;
+		//colour rectangle in image by accessing pixels; Use 
+		for (int y = trY; y < blY; y++) {
+			for (int x = blX; x < trX; x++) {
+				image.at <cv::Vec3b>(x, y) = colourVec;
+			}
+		}
+		cv::imshow("Lab 2-2", image);//redraw image on window
+		cv::waitKey(33);
+		
+		std::cout << "Press the return key exit or any other key to try again: ";
+		std::cin >> exit;
+		if (exit == '\n') {
+			cv::destroyWindow("Lab 2-2");
+			break;
+		}		
 	}
-
 	return 0;
-
 }
